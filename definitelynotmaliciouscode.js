f4eef628585c29286277b8ec1437d7f0b966233c
Dartegnian <Dartegnian@users.noreply.github.com>
2017-01-11 23:45:12 +0800

    Update definitelynotmaliciouscode.js

--- definitelynotmaliciouscode.js
+++ definitelynotmaliciouscode.js
@@ -5,7 +5,7 @@ var Seohyun = "This browser console is a feature intended for web developers.. l
 var Jessica = "This is not a \"hidden hacking tool.\"";
 var Taeyeon = "DO NOT enter or run code that you don't understand.";
 
-var YouTubeWatch_cEc2Ng26nCw = "Btw, stick around for a few more seconds."; //stay with me, last forever (yeah~)
+var YouTubeWatch_cEc2Ng26nCw = "Btw, stick around for a few more seconds."; //stay with me, last forever (yeah)
 
 var car = 0;
 var mina = 0;
@@ -25,6 +25,7 @@ var messages = [
     "The way that the bottle moves through different backgrounds,",
     "Passing through SNSD's previous concepts.",
     "Argh, I love it so much.",
+    "Here's the video, by the way: https://www.youtube.com/watch?v=UlhR0zBFxZE",
     "I should do a similar thing with my blog.",
     "When I turn 18, I'll make a post in a similar fashion.",
     "Going through the various stages in my life,",
@@ -41,12 +42,23 @@ var messages = [
     "What brings you here anyway?",
     "I mean, what brings you to my blog?",
     "Nothing's really worth checking out here, in my opinion.",
-    "I just post here from time to time because I don't want to post on Facebook",
-    "And also because Twitter has that 140 character limit.",
+    "My blog is just your average, basic blog",
+    "that's hosted on Blogger and powered by other Google services.",
+    "To be honest, I kinda love Blogger,",
+    "It's so easy to use and customizable.",
+    "I learned about Blogger in 2013",
+    "And I made my \"controversial\" first blog.",
+    "It garnered a lot of attention..",
+    "And backlash, lol.",
+    "It's still up, so you can check it out—IF you can find it.",
+    "But man, my posts were so embarrassing back then.",
+    "Anyway, I just post here from time to time because I don't want to post on Facebook",
+    "And also because Twitter has that 140-character limit.",
     "Oh, since you're here, I'm sure you won't mind",
     "If I thank certain persons in my life.",
     "To all of whom have helped me become who I am today:",
     "First on the list is my EGRAMCM professor, Miss Mina.",
+    "She's inspired me in ways I might never tell her.",
     "(She's a really, really kind and supportive professor.)",
     "(Plus she's SUPER cute lmao.)",
     "(Is she of Korean descent?)",
@@ -54,22 +66,89 @@ var messages = [
     "(Or is she a pure Filipina?)",
     "(Either way, she's ADORBZ lol.)",
     "#professorgoals",
-    "Next on the list is Sir Jeff.",
-    "Who's been the kindest teacher to me thus far.",
-    "That guy is so cool.",
-    "Who else? Oh, um, David Malan from Harvard University.",
-    "I LOVE his CS50 lessons.",
-    "Hmmm who else...",
+    "Who else? Oh, um, definitely",
+    "Justin Parado",
+    "Nicole Lopez",
+    "Nathan Melad",
+    "Rene Valguna",
+    "JR Pasaoa",
+    "Andrey Buzon",
+    "John Bautista",
+    "Samantha Munar",
+    "Especially those people because they almost always help me out during lunch.",
+    "Like, that's so kind of you all.",
+    "When I published my blog post, I didn't know that I'd receive help immediately.",
+    "(I don't expect for anything when I write, honestly. I just blog to remove my stress.)",
+    "Still, I was extremely grateful that day to finally eat lunch after weeks of not eating at APC.",
+    "I wish I can go back in time and sincerely thank you all for the support.",
+    "I know that I'm not really close to much of you",
+    "But seriously, I didn't expect that the people who I didn't talk to much",
+    "Were the ones who cared for me the most.",
+    "Justin, jeez man, the amount of help you give me is a lot.",
+    "I wish I can help you in one way or another",
+    "I'm sorry to Samantha for almost always being annoying to her",
+    "And to Nathan as well, even though I was mean to him before.",
+    "He can be a little weird at times, but he's a cool lad, he cooks great French toast.",
+    "I don't want to leave APC disliking anyone in my block.",
+    "Shout out to my gaming buddies",
+    "James Baldelomar",
+    "Joshua Morales (damn hacker)",
+    "Jordan Fernandez",
+    "Brian Olores",
+    "JR Pasaoa",
+    "Jason Adriano",
+    "Andrey Buzon",
+    "Vincent Huliganga",
+    "and Nathan Melad",
+    "Sorry mates if I'm almost always the bottom fragger.",
+    "The PP-Bizon is the only weapon I'm \"moderately\" good at :^)",
+    "I want to say thanks to James, he's been a great gaming friend.",
+    "That guy introduced me to Titan, Viggo, RoG, and a lot of new places.",
+    "I laughed at that one time he got called out for hitting the mouse too hard.",
+    "Oy Paswa ibigay mo yung bomba sakin para maka-MVP ako haha.",
+    "Natawa siguro kayo ng sobra nung natapilok ako nung papunta ng Viggo.",
+    "Badtrip yung araw na yun, haha.",
+    "Oh, and I also want to thank",
+    "all of SS151!",
+    "I fucking miss you all so much.",
+    "There isn't a day that goes by where I wouldn't wish",
+    "That I was back in APC, sitting through another boring lesson, with you all by my side.",
+    "Fuck life, but I guess things happen for a reason.",
+    "I hope you're all doing just fine there (I'm sure).",
+    "Even without your annoying class clown, who always tries to make a joke, \"banat,\" or something.",
+    "Sorry if I'm almost always hyperactive and seeming like a complete prankster.",
+    "I'm just supper happy to be with you all, ever since Day 1 with Miss Minglana.",
+    "Hey, at least I won't leave APC knowing that I'm that shy, weird guy who doesn't talk to anyone.",
+    "(Because that's who I was in high school.)",
+    "Despite not being able to meet you soon..",
+    "I'll always keep you all in my memory.",
+    "(Ew, cheezy stuff.)",
+    "But seriously",
+    "I'm glad to have people in my life that makes saying goodbye hard.",
+    ":)",
+    "..*sniffs*..",
+    "Ahem, who else...",
     "My bros, Loy and JR.",
     "They've been with me through thick and thin.",
     "Especially Loy, him and I have a long story behind us.",
-    "and, hopefully, the same is true for the future.",
+    "Both of you mean the world to me",
+    "and both of your families as well.",
+    "Never in my life have I ever thought I'd say",
+    "that I have a full and complete family.",
+    "I might not have A DAD",
+    "But I'm glad I have TWO dads.",
+    "The thing is, I don't really have a need for my biological dad.",
+    "I'd rather have a \"surrogate dad\" who's really nice and treats me like his own",
+    "Over a deceitful, two-timing, indifferent biological dad ANY DAY.",
+    "Seriously dudes, I appreciate a lot of your help.",
+    "I'm sorry if I don't show it often, because I'm shy.",
     "The same applies to my other bros as well.",
     "I'm talking about you, RC Crew.",	
-    "Oh, and Girls' Generation.. I-I mean SNSD (it sounds less girly that way).",
+    "Oh, and Girls' Generation",
+    "..I-I mean SNSD (it sounds less girly that way).",
     "They're a Korean girl group and I shouldn't be liking them, I know.",
     "But dang dude, the girls there are so...",
-    "yeppuda, haha.",
+    "neomu yeppuda, haha.",
     "Also, I've been a Sone since, like, forever.",
     "So I'm inclined to say this:",
     "Jigeumeun SONEyeoshidae~",
@@ -103,7 +182,7 @@ var messages = [
     "And I'm pretty sure you've dozed off by now lmao.",
     "I'm going to end it here.",
     "Annyeong! Really, really good goodbye.",
-    "If you're seeing this, you better get something checked in your life. Just sayin'."
+    "You're still here? Go home. Show's over. Go."
 ];
 
 var ABetterConsole = function() {
@@ -114,7 +193,7 @@ var ABetterConsole = function() {
     var HarvardAcceptanceLetterArrival;
 
     if (mina++ < messages.length) {
-		car += 1;
+        car += 1;
         AAGGParadiseInPhuket = messages[mina - 1];
         if (mina === messages.length) {
             HarvardAcceptanceLetterArrival = 999 * 9; //Machi expresscheoreom!
@@ -124,7 +203,7 @@ var ABetterConsole = function() {
         }
     }
     if (car === messages.length) {
-        setTimeout(function() {throw Error("Ijeneun annyeong. Really, really good goodbye.")}, 5000); //an SNSD-related error message
+        setTimeout(function() {throw Error("Ijeneun annyeong. Really, really good goodbye.")}, 3000); //an SNSD-related error message
     }
     else {
         console[mina == MyFavoriteProfessor ? OfCourse : Obviously](AAGGParadiseInPhuket);
@@ -138,6 +217,5 @@ console.log("%c%s", "font: 64px Open Sans; font-weight: 300; color: #0099ff; fon
 console.log("%c%s", "color: red; font-size: 15px;", Seohyun);
 console.log("%c%s", "color: red; font-size: 15px;", Jessica);
 console.log("%c%s", "color: red; font-size: 15px;", Taeyeon);
-
 console.log("%c%s", "color: red; font-size: 15px;", YouTubeWatch_cEc2Ng26nCw);
 setTimeout(ABetterConsole, 19000);
