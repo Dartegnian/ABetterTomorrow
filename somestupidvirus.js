89bfaf1a6583a5d2b147c094d7cfcbbd51070676
Dartegnian Velarde <dartegnian@users.noreply.github.com>
2017-12-04 15:30:22 +0800

    Minor code changes

--- somestupidvirus.js
+++ somestupidvirus.js
@@ -11,121 +11,121 @@
 
 function HelloSoshi()
 {
-		var mistake = null;
-		var bluejeans = document.title;
-        var howgreatisyourlove =
-        [
-			/* Seohyun - I'll Wait For You */
-			"I can't be seen, my words can't be heard",
-			"But I am here, I will be waiting for you",
-			"I am at the place where your memory remains",
-			"Even when time and tears flow, I will only be waiting for you",
+	var mistake = null;
+	var bluejeans = document.title;
+    var howgreatisyourlove =
+    [
+		/* Seohyun - I'll Wait For You */
+		"I can't be seen, my words can't be heard",
+		"But I am here, I will be waiting for you",
+		"I am at the place where your memory remains",
+		"Even when time and tears flow, I will only be waiting for you",
 
-			/* Girls' Generation - Tears */
-			"Come to me as the wind",
-			"Come back to me as the rain",
-			"I only think about you",
+		/* Girls' Generation - Tears */
+		"Come to me as the wind",
+		"Come back to me as the rain",
+		"I only think about you",
 
-			/* Tiffany - Yellow Light */
-			"If I wait a bit more, will you come back to your spot?",
+		/* Tiffany - Yellow Light */
+		"If I wait a bit more, will you come back to your spot?",
 
-			/* Girls' Generation - Snowy Wish */
-			"I felt lonely by myself",
+		/* Girls' Generation - Snowy Wish */
+		"I felt lonely by myself",
 
-			/* Sunny - Second Drawer */ /* <3 http://dartegnian.weebly.com */
-			"Again today, I ask myself how I'm doing",
-			"The dreams that I borrowed from this world",
-			"They are wearing out day by day",
-			"Will I be okay?",
-			"Can I live with a heart that is like a star that never falls?",
-			"Can I live with a heart that doesn't get wet from the rain?",
-			"Is my hurt heart healing a little?",
-			"If I wait again, if I go again, will we be together?",
-			"I'm okay because I have you",
-			
-			/* Taeyeon - Lonely Night */
-			"It’s time I get used to it",
-			"But the night is so long, I can’t fall asleep",
-			"What are you doing right now?",
-			"I wanna go someplace unfamiliar",
-			"I know, that you’re not here",
-			"I’m sick of lonely nights",
+		/* Sunny - Second Drawer */ /* <3 http://dartegnian.weebly.com */
+		"Again today, I ask myself how I'm doing",
+		"The dreams that I borrowed from this world",
+		"They are wearing out day by day",
+		"Will I be okay?",
+		"Can I live with a heart that is like a star that never falls?",
+		"Can I live with a heart that doesn't get wet from the rain?",
+		"Is my hurt heart healing a little?",
+		"If I wait again, if I go again, will we be together?",
+		"I'm okay because I have you",
+		
+		/* Taeyeon - Lonely Night */
+		"It’s time I get used to it",
+		"But the night is so long, I can’t fall asleep",
+		"What are you doing right now?",
+		"I wanna go someplace unfamiliar",
+		"I know, that you’re not here",
+		"I’m sick of lonely nights",
 
-			/* Girls' Generation - My Best Friend */
-			"Alright, let's make a promise. You are my best friend.",
-			"There is only one of you in this world, my friend",
-			"You're my joy and my soul",
-			"I'll continue to treasure our importance",
-			"I'll be your strength, I'll always be your best friend",
-			"Let's not be stingy and buy each other meals",
-			"For me, when you're not here, I really feel lonely",
-			"You're my life's present, my friend",
-			"The word \"friendship\" may sound awkward, but I think that word was created to describe us",
+		/* Girls' Generation - My Best Friend */
+		"Alright, let's make a promise. You are my best friend.",
+		"There is only one of you in this world, my friend",
+		"You're my joy and my soul",
+		"I'll continue to treasure our importance",
+		"I'll be your strength, I'll always be your best friend",
+		"Let's not be stingy and buy each other meals",
+		"For me, when you're not here, I really feel lonely",
+		"You're my life's present, my friend",
+		"The word \"friendship\" may sound awkward, but I think that word was created to describe us",
 
-			/* Taeyeon - The Blue Night of Jeju Island */
-			"Let’s leave, the two of us abandoning everything",
-			"Let’s leave to Jeju Island, under the blue night sky",
-			"Let’s leave, together, it’s not that difficult",
+		/* Taeyeon - The Blue Night of Jeju Island */
+		"Let’s leave, the two of us abandoning everything",
+		"Let’s leave to Jeju Island, under the blue night sky",
+		"Let’s leave, together, it’s not that difficult",
 
-			/* Jackson 5 - I Want You Back */
-			"When I had you to myself, I didn't want you around",
-			"Oh baby, give me one more chance",
-			"Won't you please let me back in your heart",
-			"Oh darlin', I was blind to let you go",
-			"I want you back",
-			"Trying to live without your love is one long sleepless night",
+		/* Jackson 5 - I Want You Back */
+		"When I had you to myself, I didn't want you around",
+		"Oh baby, give me one more chance",
+		"Won't you please let me back in your heart",
+		"Oh darlin', I was blind to let you go",
+		"I want you back",
+		"Trying to live without your love is one long sleepless night",
 
-			/*Overwatch voice lines*/
-			"AFK",
-			"这个世界值得我们奋战！",
-			"You have to let it go",
-			"I'm not a miracle worker. Well, not always.",
-			"Take two and call me in the morning",
-			"Do You Need A Hug?",
-			"Play Nice, Play Pharah",
-			"Welcome to my reality",
-			"Everything by design",
-			"People always underestimate the engineers",
-			"Peace and blessings be upon you all",
-			"Hello, world!",
-			"替え玉!",
-			"Would you like the sponge or the spray?",
-			"Hard work and dedication pays off",
-			"أنا هحميك",
-			"No le saques",
+		/*Overwatch voice lines*/
+		"AFK",
+		"这个世界值得我们奋战！",
+		"You have to let it go",
+		"I'm not a miracle worker. Well, not always.",
+		"Take two and call me in the morning",
+		"Do You Need A Hug?",
+		"Play Nice, Play Pharah",
+		"Welcome to my reality",
+		"Everything by design",
+		"People always underestimate the engineers",
+		"Peace and blessings be upon you all",
+		"Hello, world!",
+		"替え玉!",
+		"Would you like the sponge or the spray?",
+		"Hard work and dedication pays off",
+		"أنا هحميك",
+		"No le saques",
 
-			"PRAISE LORD HELIX!",
-			"I am Groot",
-			"Wew lad",
-			"And why did you click off",
-			"THE MAIN TAB HAS CHANGED",
-			"Annyeong!~"
-        ];
+		"PRAISE LORD HELIX!",
+		"I am Groot",
+		"Wew lad",
+		"And why did you click off",
+		"THE MAIN TAB HAS CHANGED",
+		"Annyeong!~"
+    ];
         
-		// check if window is moved out of focus
-		$(window).on('blur', function()
+	// check if window is moved out of focus
+	$(window).on('blur', function()
+	{
+		// pick random string from array for the title
+		document.title = howgreatisyourlove[Math.floor(Math.random() * howgreatisyourlove.length)];
+
+		// change window title every 5 seconds
+		mistake = setInterval(function() {
+		    document.title = howgreatisyourlove[Math.floor(Math.random() * howgreatisyourlove.length)];
+		}, 5000);
+	});
+
+	// check if window is in focus
+	$(window).on('focus', function()
+	{
+		if(mistake)
 		{
-			// pick random string from array for the title
-			document.title = howgreatisyourlove[Math.floor(Math.random() * howgreatisyourlove.length)];
+		    clearInterval(mistake);
+		    mistake = null;
+		}
 
-			// change window title every 5 seconds
-			mistake = setInterval(function() {
-			    document.title = howgreatisyourlove[Math.floor(Math.random() * howgreatisyourlove.length)];
-			}, 5000);
-		});
-
-		// check if window is in focus
-		$(window).on('focus', function()
-		{
-			if(mistake)
-			{
-			    clearInterval(mistake);
-			    mistake = null;
-			}
-
-			// reset tab title to original
-			document.title = bluejeans;
-		});
+		// reset tab title to original
+		document.title = bluejeans;
+	});
 }
 
 //initializes code 
