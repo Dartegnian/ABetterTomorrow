687bee8c335bdc92b924b19ce138b05a08e8661a
Dartegnian Velarde <dartegnian@users.noreply.github.com>
2018-01-05 09:33:23 +0800

    Branched code and added Monika from DDLC

---
+++ lilmonix3.js
@@ -0,0 +1,234 @@
+// initialize first messages
+var NareulMidoJugirBare = "Whoa, what are you doing here?";
+var HamkkeIsseo = "—Dartegnian";
+
+var Seohyun = "This browser console is a feature intended for web developers.. like me :^)";
+var Jessica = "This is not a \"hidden hacker tool\" nor are these \"cyber police options.\"";
+var Taeyeon = "DO NOT run code that you don't fully understand.";
+
+var YouTubeWatch_1a9vyHLxKzs = "Oh and please do stick around for a few more seconds."; //stay with me, last forever (yeah)
+
+// outputs first messages
+console.log("%c%s", "font: 44px 'Segoe UI', Helvetica, Open Sans, Arial, sans-serif; font-weight: 300; color: white; background: #0099ff;", NareulMidoJugirBare);
+console.log("%c%s", "font: 44px 'Segoe UI', Helvetica, Open Sans, Arial, sans-serif; font-weight: 300; color: #0099ff; font-size: 44px;", HamkkeIsseo);
+
+console.log("%c%s", "color: red; font-size: 15px;", Seohyun);
+console.log("%c%s", "color: red; font-size: 15px;", Jessica);
+console.log("%c%s", "color: red; font-size: 15px;", Taeyeon);
+console.log("%c%s", "color: red; font-size: 15px;", YouTubeWatch_1a9vyHLxKzs);
+
+// main script code
+var ABetterConsole = function()
+{
+	var Obviously = 'log';
+	var OfCourse = 'warn';
+	var MyFavoriteProfessor = 1;
+	var AAGGParadiseInPhuket;
+	var HarvardAcceptanceLetterArrival;
+
+	if (mina++ < messages.length)
+	{
+		car += 1;
+		AAGGParadiseInPhuket = messages[mina - 1];
+		if (mina === messages.length)
+			HarvardAcceptanceLetterArrival = 999 * 9; //Machi expresscheoreom!
+		else
+			HarvardAcceptanceLetterArrival = 1000 + AAGGParadiseInPhuket.length * 50;
+	}
+
+	if (car === messages.length)
+		setTimeout(function() {throw Error("Ijeneun annyeong. Really, really good goodbye.")}, 3000); //an SNSD-related error message
+	else
+	{
+		console[mina == MyFavoriteProfessor ? OfCourse : Obviously](AAGGParadiseInPhuket); // <--- THE MOST IMPORTANT LINE IN THIS PROGRAM
+		setTimeout(ABetterConsole, HarvardAcceptanceLetterArrival);
+	}
+};
+
+// main message script
+// chapter 3 coming soon!
+var car = 0;
+var mina = 0;
+var messages =
+[
+	"Ey",
+	"I know it's been a while.",
+	"I don’t think",
+	"we’ve got the girls out.",
+	"Woo!",
+	"We gon' break some hearts tonight!",
+	"We gon' make some noise tonight!",
+	"The girls are back!",
+	"Aw man, it does feel good to be a SONE again.",
+	"Woo!",
+	"Having the girls back,",
+	"even without Jessica,",
+	"is seriously one of the best things that has happened in my life recently.",
+	"Have you listened to Girls' Generation's new album yet?",
+	"I URGE you to listen to it. It's so good, seriously.",
+	"If you're looking to try it out to know how the rest of the album sounds,",
+	"then I suggest starting with one \"One Last Time.\"",
+	"Go on YouTube right now and search for \"one last time snsd english lyrics\"",
+	"and listen to it, plus the translations are synced up so you know what they're saying.",
+	"It's such a great song, seriously. Oh my God.",
+	"The first I listened to the song along with the translation, it made my hairs stand on end.",
+	"\"One Last Time\" is basically a song about",
+	"knowing that your relationship will come to an end soon.",
+	"The song doesn't state the goodbye of a romantic relationship.",
+	"Just any relationship wherein you loved the other person.",
+	"You know,",
+	"if I could sing this to Elaine (in English)",
+	"I would do it because I miss her so much.",
+	"Some of the lines in the song can really strike a chord with me.",
+	"\"If you wanted to change, I wanted to stay the same.\"",
+	"\"Over time, the two steps that were side by side",
+	"are out of step.\"",
+	"\"Tears fall, I’m scared we’ll become distant.\"",
+	"and ESPECIALLY",
+	"\"Many days and nights we were together,",
+	"don’t forget all of it.\"",
+	"Some lines from \"Light up the Sky\" also apply to Elaine.",
+	"...",
+	"I miss Elaine.",
+	"I'm happy because of SNSD's 10th anniversary comeback, being a SONE again, and all that.",
+	"But this comeback..",
+	"I don't know, it just feels empty without Elaine.",
+	"The last time I was with her and SNSD released something new",
+	"was \"Mr. Mr.\", and I even changed her blog theme here on Blogger to have the aesthetic of that album.",
+	"Man, I miss Elaine so much.",
+	"She was the best friend I could ever ask for.",
+	"In the year 2013, that's when I really felt Elaine's presence in my life.",
+	"I had extreme problems on that year.",
+	"I was depressed, I was angry, I felt unloved",
+	"I-I felt lonely.",
+	"Like no one was there. No one gave two shits about me.",
+	"I wanted to commit suicide that year because the pain was too unbearable for me",
+	"and no one gave a shit about my mental health.",
+	"Everyone who knew about my situation just thought I was crazy",
+	"and they were quick to judge, no one cared about why I was acting like that",
+	"and no one wanted to help.",
+	"Well, except for one person",
+	"Elaine.",
+	"What she did helped me so much.",
+	"At a time when no one else cared,",
+	"she was the only one who did.",
+	"She made me laugh at a time when I was depressed.",
+	"She made me smile just by spazzing to me about news from SNSD.",
+	"But, most of all,",
+	"she made me feel loved.",
+	"The world wasn't as cold as it seemed anymore",
+	"and, well, through her, I slowly recovered.",
+	"I mean, sure, there were times when I relapsed back into depression",
+	"but she was always there to bring me back.",
+	"I would tell her about my accomplishments and stuff I did",
+	"in return, I'd listen to her and hear more about her life.",
+	"The things she did will always stay in my heart.",
+	"Most people in the world can be cruel, and some just.. indifferent.",
+	"\"Oh? You're depressed? Okay, whatever, I don't fucking care.\"",
+	"But, Elaine is different.",
+	"I personally don't know why she did the things she did for me.",
+	"I don't know why she decided to cheer me up, help me, make me laugh,",
+	"sometimes she slapped some sense into me.",
+	"I know she's talked to my mom and she talked to me about my family problems at one point.",
+	"I'm sure it was stressful on her part because I was so stubborn",
+	"but we eventually came over that",
+	"and we slowly became the best of friends.",
+	"Even so far as to thinking that the other is a long-lost sibling.",
+	"But then, late 2014 happened.",
+	"With Jessica leaving the group, and with SNSD winding down their activities,",
+	"Elaine and I didn't have much to talk about anymore.",
+	"We just.. grew apart.",
+	"I explained my feelings of missing her in my \"Missing Things\" post, as well in my other posts.",
+	"I really do miss her.",
+	"I miss having that someone to talk to, someone to share my story with",
+	"..that shoulder to cry on, that someone to cheer you up.",
+	"Meeting someone like her again is hard.",
+	"But I'd do anything to be with my big sister Elaine again.",
+	"If I could only go back to 2013",
+	"just for one year,",
+	"even if I have to throw everything away, I'd do it.",
+	"My story with her might be sad, sure, but it's not all sad.",
+	"I could be a sadder sack of shit right now if it wasn't for her",
+	"or, you know, I could be dead right now.",
+	"Who knows? I could still be alive, but look at the world as this cold, barren, piece of shit place",
+	"and I'd hate everyone.",
+	"But.. I don't, and I think everyone has some form of good within them.",
+	"And I look at the world in a better light. The world might seem doomed, but I have hope.",
+	"I believe in the possibility of a better tomorrow.",
+	"Elaine may be gone from my life, but the things she left behind still linger with me.",
+	"She just made me a better person",
+	"and I love her for that.",
+	"(Not in a romantic way, just to get that out. In a way that a brother would tell his big sister.)",
+	"Elaine wasn't the only hero in my life, though.",
+	"There are countless others.",
+	"ESPECIALLY the people in my block.",
+	"I don't show it that much, and I doubt my blockmates know this",
+	"but I fucking love my block.",
+	"They're seriously the best and most fun group of classmates I've ever been in.",
+	"They helped and chipped in for me at time when I needed it.",
+	"Some people gave me lunch when I had nothing to eat.",
+	"I remember this one specific event",
+	"the day after I posted about my current financial situation.",
+	"A friend of mine gave me money to eat lunch",
+	"and I've been starving that whole week.",
+	"To be honest, I felt like crying when I learned that my blockmates gave me money just to eat for the day.",
+	"Ah, my second term in my second year was a tough one",
+	"but I've got friends",
+	"so I got through it.",
+	"And, because they deserved it, I personally would like to thank, from the bottom of my heart",
+	"these awesome blockmates for having helped me during tough times, in no particular order:",
+	"Justin Parado",
+	"Nicole Lopez",
+	"Nathan Melad",
+	"James Baldelomar",
+	"Rene Valguna",
+	"Samantha Munar",
+	"Andrey Buzon",
+	"John Bautista",
+	"and Hugo Dimaranan",
+	"(oh, and the entire block of SS151).",
+	"They're not the only ones who helped me, though. My org family, APC Speaks, helped me as well.",
+	"Even Loy's family decided to help me out by basically covering my education.",
+	"Countless other individuals also helped me, Paula Vesliño, Kenard Pascua, Ate Betina Dela Cruz, Andrea Aguillon, Ate Ding and Ate Ching Parado, and so much more.",
+	"The truth is, I wasn't not born rich, and I was born into a depressing life.",
+	"Horrible, abusive dad at early childhood,",
+	"separation of parents at an early age,",
+	"and being put into a family that doesn't give a shit about you.",
+	"Oh, and the alcoholic uncle that slams your door and says mean shit to you every month.",
+	"Plus add the selfish-as-hell Dad that won't help you as a free DLC.",
+	"I wasn't really supposed to amount to anything, and my destiny would've been a hard life.",
+	"Were it not for all of the people that have helped me and put faith in me.",
+	"If no one really cared about me, if no one really gave a shit about me",
+	"then, for one, I wouldn't be studying, I would've dropped out of my second year in college.",
+	"And I wouldn't drop out on a light note as well, I would drop out as an infuriated and depressed kid that didn't talk to anyone.",
+	"With a broken family, a broken heart, and a broken future",
+	"I would probably commit suicide then and there.",
+	"I would be dead",
+	"but.. I didn't.",
+	"I'm studying, I'm happy, and I've got people helping me.",
+	"I wasn't born into a \"normal family\"",
+	"but I'm glad that the family I have now has so many members, it's hard to count.",
+	"They all pitched in and made me who I am today.",
+	"My mission in life is to really make the world a better place.",
+	"Not because I want fame or notoriety",
+	"but I just want to pay the kindness forward.",
+	"And I want to help others as much as others have helped me.",
+	"I'm still quite young, and I still have a lot to learn",
+	"still I have a brave soul, and I doubt I'll be going quietly into the night soon.",
+	"The moral of my story thus far?",
+	"",
+	"Always be good.",
+	"Be understanding.",
+	"Be kind.",
+	"Be that friend who everyone can cry on and cheer others up.",
+	"Just be a kind person.",
+	"That's all. I have to say goodbye now. I hope you had fun reading as much as I did writing.",
+	"Feel free to leave a comment or talk to me, if ever.",
+	"I'll be there.",
+	"Until next time! And in my SONE-way of saying goodbye...",
+	"Annyeong! Really, really good goodbye.",
+	"If you or a loved one has been diagnosed with Mesothelioma you may to be entitled to financial compensation.",
+];
+
+// initializer, run ABetterConsole function after 19 seconds
+setTimeout(ABetterConsole, 19000);
