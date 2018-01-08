7533fa074f5a9fad45f2c44b7410dfbbf6564583
Dartegnian Velarde <dartegnian@users.noreply.github.com>
2018-01-08 23:28:31 +0800

    Restructured song list

--- somestupidvirus.js
+++ somestupidvirus.js
@@ -38,12 +38,6 @@ function HelloSoshi()
 		"And in your reality, if I don't know how to love you",
 		"I'll leave you be",
 
-		/* Seohyun - I'll Wait For You */
-		"I can't be seen, my words can't be heard",
-		"But I am here, I will be waiting for you",
-		"I am at the place where your memory remains",
-		"Even when time and tears flow, I will only be waiting for you",
-
 		/* Girls' Generation - Tears */
 		"Come to me as the wind",
 		"Come back to me as the rain",
@@ -52,8 +46,22 @@ function HelloSoshi()
 		"You leave so many memories",
 		"How can I convey it to you?",
 
-		/* Tiffany - Yellow Light */
-		"If I wait a bit more, will you come back to your spot?",
+		/* Girls Generation - My Child/Fairytale */
+		"I draw of the future days of just you and I",
+		"Let's fill a treasure chest full with our wishes",
+		"And hide it underneath the tree that we chose",
+		"The fairy tale of you and I",
+
+		/* Girls' Generation - My Best Friend */
+		"Alright, let's make a promise. You are my best friend.",
+		"There is only one of you in this world, my friend",
+		"You're my joy and my soul",
+		"I'll continue to treasure our importance",
+		"I'll be your strength, I'll always be your best friend",
+		"Let's not be stingy and buy each other meals",
+		"For me, when you're not here, I really feel lonely",
+		"You're my life's present, my friend",
+		"The word \"friendship\" may sound awkward, but I think that word was created to describe us",
 
 		/* Girls' Generation - Snowy Wish */
 		"I felt lonely by myself",
@@ -76,7 +84,13 @@ function HelloSoshi()
 		"Is my hurt heart healing a little?",
 		"If I wait again, if I go again, will we be together?",
 		"I'm okay because I have you",
-		
+
+		/* Seohyun - I'll Wait For You */
+		"I can't be seen, my words can't be heard",
+		"But I am here, I will be waiting for you",
+		"I am at the place where your memory remains",
+		"Even when time and tears flow, I will only be waiting for you",
+
 		/* Taeyeon - Lonely Night */
 		"It's time I get used to it",
 		"But the night is so long, I can't fall asleep",
@@ -84,28 +98,14 @@ function HelloSoshi()
 		"I wanna go someplace unfamiliar",
 		"I know, that you're not here",
 		"I'm sick of lonely nights",
-
-		/* Girls Generation - My Child/Fairytale */
-		"I draw of the future days of just you and I",
-		"Let's fill a treasure chest full with our wishes",
-		"And hide it underneath the tree that we chose",
-		"The fairy tale of you and I",
-
-		/* Girls' Generation - My Best Friend */
-		"Alright, let's make a promise. You are my best friend.",
-		"There is only one of you in this world, my friend",
-		"You're my joy and my soul",
-		"I'll continue to treasure our importance",
-		"I'll be your strength, I'll always be your best friend",
-		"Let's not be stingy and buy each other meals",
-		"For me, when you're not here, I really feel lonely",
-		"You're my life's present, my friend",
-		"The word \"friendship\" may sound awkward, but I think that word was created to describe us",
-
+		
 		/* Taeyeon - The Blue Night of Jeju Island */
 		"Let's leave, the two of us abandoning everything",
 		"Let's leave to Jeju Island, under the blue night sky",
 		"Let's leave, together, it's not that difficult",
+		
+		/* Tiffany - Yellow Light */
+		"If I wait a bit more, will you come back to your spot?",
 
 		/* Jackson 5 - I Want You Back */
 		"When I had you to myself, I didn't want you around",
